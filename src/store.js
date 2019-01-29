d62b09d8233a15bd831b1ebc023af4849fdcc3e2
James Dindi <dindijjames@gmail.com>
2019-01-29 14:39:31 +0300

    Style the components

--- src/store.js
+++ src/store.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    title: 'My Custom Title',
+    links: [
+      'https://www.marvelholidays.com',
+      'https://www.marvelin.com',
+      'https://www.marvelair.com',
+      'https://www.marvelgolf.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
 
